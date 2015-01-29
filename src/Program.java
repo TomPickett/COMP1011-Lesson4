d1f29cb7048f019139c7d555abd48479961a844c
TomPickett <Pickett.tom7@gmail.com>
2015-01-29 17:02:52 -0500

    Completed Lesson 4

--- src/Program.java
+++ src/Program.java
@@ -4,11 +4,38 @@
  *
  */
 
+import javax.swing.JOptionPane;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstNumber = "";
+		String secondNumber = "";
+		
+		
 
+		
+		try {
+			
+			firstNumber = JOptionPane.showInputDialog("Enter first integer");
+			System.out.println(firstNumber);
+			secondNumber = JOptionPane.showInputDialog("Enter first integer");
+			System.out.println(secondNumber);
+			
+			
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);
+			
+			int sum = number1 + number2;
+			
+			JOptionPane.showMessageDialog(null, "The Sum is " + sum + "Sum of two integers", secondNumber, JOptionPane.PLAIN_MESSAGE);
+			
+		} catch (Exception e){
+			JOptionPane.showMessageDialog(null, "Incorrect Values Entered.");
+			JOptionPane.showMessageDialog(null, "Incorrect Values Entered", "Entry Error", JOptionPane.ERROR_MESSAGE);
+			
+			System.out.println(e);
+		}
 	}
-
 }
